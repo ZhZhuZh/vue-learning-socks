630319753fe8924f59ff0490fd96a62c7c524eaf
Виктория Жукова <viktoria.zhukova@bergen.tech>
2023-08-09 18:44:19 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            cart: [],
+            premium: false
+        }
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeProduct(id) {
+            this.cart.pop(id)
+        }
+    }
+})
